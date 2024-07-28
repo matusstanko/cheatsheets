c24b634046d0823f42d82026a41acac47a523520
matusstanko <matus.stanko18@gmail.com>
2024-07-28 19:48:38 +0200

    Update JS cheatsheet with objects, setters and getters, factory functions, destructured assignment, and built-in object methods

--- JS_Cheatsheet.js
+++ JS_Cheatsheet.js
@@ -176,3 +176,110 @@ for (let crew_member in spaceship.crew) {
 console.log(`${crew_member}: ${spaceship.crew[crew_member].name}`)
 }
 
+// SETTERS and GETTERS
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  get numOfSensors(){
+    if(typeof this._numOfSensors === 'number'){
+      return this._numOfSensors;
+    } else {
+      return 'Sensors are currently down.'
+    }
+  },
+  set numOfSensors(num){
+    if (typeof num === 'number' && num >= 0){
+      this._numOfSensors = num
+    } else {
+      console.log('Pass in a number that is greater than or equal to 0')
+    }
+  }
+};
+
+robot.numOfSensors = 100
+console.log(robot.numOfSensors)
+
+
+// Factory functions 
+
+// Old (long)
+const robotFactory = (model, mobile) => {
+  return{
+    model: model,
+    mobile: mobile,
+    beep (){
+      console.log('Beep Boop')
+    }
+  }
+}
+
+const tinCan = robotFactory('P-500',true)
+
+tinCan.beep()
+// New (shorter)
+const robotFactory = (model, mobile) => {
+  return {
+    model,
+    mobile,
+    beep() {
+      console.log('Beep Boop');
+    }
+  }
+}
+
+// Destructured Assignment
+const robot = {
+  model: '1E78V2',
+  energyLevel: 100,
+  functionality: {
+    beep() {
+      console.log('Beep Boop');
+    },
+    fireLaser() {
+      console.log('Pew Pew');
+    },
+  }
+};
+
+const {functionality} = robot
+functionality.beep()
+
+// Built-in object methods
+const robot = {
+	model: 'SAL-1000',
+  mobile: true,
+  sentient: false,
+  armor: 'Steel-plated',
+  energyLevel: 75
+};
+
+// What is missing in the following method call?
+const robotKeys = Object.keys(robot);
+
+console.log(robotKeys);
+
+// Declare robotEntries below this line:
+const robotEntries = Object.entries(robot)
+
+console.log(robotEntries);
+// Declare newRobot below this line:
+const newRobot = Object.assign({laserBlaster: true, voiceRecognition: true}, robot)
+
+console.log(newRobot);
+// OUTPUT:
+/*
+[ 'model', 'mobile', 'sentient', 'armor', 'energyLevel' ]
+[ [ 'model', 'SAL-1000' ],
+  [ 'mobile', true ],
+  [ 'sentient', false ],
+  [ 'armor', 'Steel-plated' ],
+  [ 'energyLevel', 75 ] ]
+{ laserBlaster: true,
+  voiceRecognition: true,
+  model: 'SAL-1000',
+  mobile: true,
+  sentient: false,
+  armor: 'Steel-plated',
+  energyLevel: 75 }
+*/

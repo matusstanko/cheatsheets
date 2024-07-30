b2151f8b2d573377ba4088f08b550f32e8f4c8d7
matusstanko <matus.stanko18@gmail.com>
2024-07-30 14:56:46 +0200

    add: typescript cheatsheet update

--- TypeScript_Cheatsheet.ts
+++ TypeScript_Cheatsheet.ts
@@ -92,3 +92,71 @@ type Student = {
   };
   
   let boris: Student = {name: 'Boris', age: 35, courses: ['JavaScript', 'TypeScript']};
+
+  
+// Function types: To define my own type
+ function add(a, b){return a+b }
+
+ type OperatorFunction = (a: number, b: number) => number //Define type, where input are 2 number, output number
+ 
+ function mathTutor(operationCallback: OperatorFunction) { // Set type to OperatorFunction!
+   console.log("Let's learn how to", operationCallback.name,'!');
+   let value25 = operationCallback(2,5);
+   console.log('When we', operationCallback.name, '2 and 5, we get', value25, '.');
+   console.log('When we', operationCallback.name, value25, 'and 7, we get', operationCallback(value25,7), '.');
+   console.log('Now fill out this worksheet.');
+ }
+ 
+
+// Generic types: To define type and also structure of type
+type Human = {name: string, job: string};
+type Dog = {name: string, tailWagSpeed: number};
+type Family<T> = { // firstly list of 2 things, then thing and then thing list (size not defined)
+  parents: [T, T], mate: T, children: T[]
+};
+
+let theFamily: Family<number> = {
+  parents: [3, 4], mate: 9, children: [5, 30, 121]
+};
+
+let aFamily: Family<Human> = { // Type is Family, but now we have object with name, job
+  parents: [
+    {name: 'Mom', job: 'software engineer'},
+    {name: 'Dad', job: 'coding engineer'}
+  ],
+  mate: {name: 'Matesky', job: 'engineering coder'},
+  children: [{name: 'Babesky', job: 'none'}]
+};
+
+let anotherFamily: Family<Dog> = { // Type is Family, but now we have object with name, tailWagSpeed
+  parents: [
+    {name: 'Momo', tailWagSpeed: 3},
+    {name: 'Dado', tailWagSpeed: 100}
+  ],
+  mate: {name: 'Cheems', tailWagSpeed: 7},
+  children: [
+    {name: 'Puppin', tailWagSpeed: 0.001},
+    {name: 'Puppenaut', tailWagSpeed: 0.0001},
+    {name: 'Puppenator', tailWagSpeed: 0.01}
+  ]
+};
+
+
+// Generic functions: To use more types in one function
+function getFilledArray<T>(value: T, n: number): T[] { // value is type of T 
+  return Array(n).fill(value);
+}
+
+let stringArray: string[];
+let numberArray: number[];
+let personArray: {name: string, age: number}[];
+let coordinateArray: [number, number][];
+
+stringArray = getFilledArray<string>('hi',6); // value parameter to function is <string>
+numberArray = getFilledArray<number>(9,6); // value parameter is number
+personArray = getFilledArray<{name: string, age: number}>({name: 'J. Dean', age: 24}, 6); // object with name, age as numbers
+coordinateArray = getFilledArray<[number, number]>([3,4],6); // array with two numbers
+
+
+
+ 
